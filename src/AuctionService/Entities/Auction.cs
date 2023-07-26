492e8f00241d36b369d7dc5187aebc5c8f777a24
Rafael Duarte de Medeiros <rafael.medeiros@amlabs.com.br>
2023-07-26 17:32:24 -0300

    Added Auction Types

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,28 @@
+﻿namespace AuctionService.Entities;
+
+public class Auction
+{
+    public Guid Id { get; set; }
+    
+    public string Seller { get; set; } = null!;
+    public string Winner { get; set; } = null!;
+
+    public decimal ReservePrice { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHigBid { get; set; }
+    
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime? Updated { get; set; }
+    public DateTime? AuctionEnded { get; set; }
+    
+    public AuctionStatus Status { get; set; } = AuctionStatus.Live;
+    
+    public AuctionItem Item { get; set; } = null!;
+}
+
+public enum AuctionStatus
+{
+    Live,
+    Finished,
+    ReservedNotMet,
+}
---
+++ src/AuctionService/Entities/AuctionItem.cs
@@ -0,0 +1,27 @@
+﻿namespace AuctionService.Entities;
+
+public class AuctionItem
+{
+    public Guid Id { get; set; }
+
+    public string Model { get; set; } = null!;
+    public string Manufacturer { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public AuctionItemType Type { get; set; }
+    public string? SerialNumber { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; } = null!;
+    public string ImageUrl { get; set; } = null!;
+
+    public Auction Auction { get; set; } = null!;
+    public Guid AuctionId { get; set; }
+}
+
+public enum AuctionItemType
+{
+    Guitar,
+    Amplifier,
+    Pedal,
+    MultiEffect,
+    BassGuitar
+}
