0bd2343d73414bfe7a2237c01d743ef8206d2561
Rafael Duarte de Medeiros <rafael.medeiros@amlabs.com.br>
2023-07-26 18:46:30 -0300

    Added first migration

---
+++ src/AuctionService/Data/Migrations/20230726210619_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Auctions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Seller = table.Column<string>(type: "text", nullable: false),
+                    Winner = table.Column<string>(type: "text", nullable: false),
+                    ReservePrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    SoldAmount = table.Column<int>(type: "integer", nullable: true),
+                    CurrentHigBid = table.Column<int>(type: "integer", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Updated = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    AuctionEnded = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Auctions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuctionItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    Manufacturer = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    SerialNumber = table.Column<string>(type: "text", nullable: true),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    Color = table.Column<string>(type: "text", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false),
+                    AuctionId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuctionItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AuctionItems_Auctions_AuctionId",
+                        column: x => x.AuctionId,
+                        principalTable: "Auctions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuctionItems_AuctionId",
+                table: "AuctionItems",
+                column: "AuctionId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuctionItems");
+
+            migrationBuilder.DropTable(
+                name: "Auctions");
+        }
+    }
+}
--- src/AuctionService/Entities/AuctionItem.cs
+++ src/AuctionService/Entities/AuctionItem.cs
@@ -8,9 +8,11 @@ public class AuctionItem
     public string Manufacturer { get; set; } = null!;
     public string Description { get; set; } = null!;
     public AuctionItemType Type { get; set; }
+    
     public string? SerialNumber { get; set; }
     public int Year { get; set; }
     public string Color { get; set; } = null!;
+
     public string ImageUrl { get; set; } = null!;
 
     public Auction Auction { get; set; } = null!;
