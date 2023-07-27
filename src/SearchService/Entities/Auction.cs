e33a1c3cb7192a6364e3a64190e4f063488d08ec
Rafael Duarte de Medeiros <rafael.medeiros@amlabs.com.br>
2023-07-26 23:05:47 -0300

    Added mongo db

---
+++ src/SearchService/Entities/Auction.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService.Entities;
+
+public class Auction : Entity
+{
+    public string Seller { get; set; } = null!;
+    public string? Winner { get; set; }
+    public decimal ReservePrice { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHigBid { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+    public DateTime? AuctionEnded { get; set; }
+    public string Status { get; set; } = null!;
+    
+    public string Model { get; set; } = null!;
+    public string Manufacturer { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Type { get; set; } = null!;
+    public string? SerialNumber { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; } = null!;
+    public string? ImageUrl { get; set; }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +15,13 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDb",
+    MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Auction>()
+    .Key(a => a.Model, KeyType.Text)
+    .Key(a => a.Manufacturer, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
