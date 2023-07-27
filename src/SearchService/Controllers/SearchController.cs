bad1191a843ebdb7c762270e111517c66366a0fe
Rafael Duarte de Medeiros <rafael.medeiros@amlabs.com.br>
2023-07-27 18:23:24 -0300

    Added pagination

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -9,15 +9,22 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Auction>>> SearchAuctions([FromQuery] string? searchTerm)
+    public async Task<ActionResult<List<Auction>>> SearchAuctions([FromQuery] string? searchTerm, int page = 1, int pageSize = 5)
     {
-        var query = DB.Find<Auction>();
+        var query = DB.PagedSearch<Auction>();
         query.Sort(auction => auction.Ascending(field => field.AuctionEnd));
         
         if (!string.IsNullOrEmpty(searchTerm))
             query.Match(Search.Full, searchTerm).SortByTextScore();
         
+        query.PageNumber(page).PageSize(pageSize);
+        
         var results = await query.ExecuteAsync();
-        return Ok(results);
+        return Ok(new
+        {
+            results = results.Results,
+            pageCount = results.PageCount,
+            totalCount = results.TotalCount,
+        });
     }
 }
