4eb0900decd8d956e4a1ff0f6a6891c7d1858c5b
rafaelmmedeiros <rafael.einherjar@gmail.com>
2023-08-29 16:18:08 -0300

    Added CountdownTimer.tsx

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,5 +1,6 @@
 ﻿import Image from "next/image";
 import React from "react";
+import CountdownTimer from "@/app/auctions/CountdownTimer";
 
 type Props = {
   auction: any
@@ -16,13 +17,18 @@ export default function AuctionCard({auction}: Props) {
             fill
             priority
             className={'object-cover'}
-            sizes={'(max-width: 768px) 100vw, (max-width: 1200px) 50vw'}/>
+            sizes={'(max-width: 768px) 100vw, (max-width: 1200px) 50vw'}
+          />
+          <div className={'absolute bottom-2 left-2'}>
+            <CountdownTimer auctionEnd={auction.auctionEnd}/>
+          </div>
         </div>
       </div>
       <div className={'flex justify-between items-center mt-4'}>
         <h3 className={'text-gray-700'}>{auction.brand} {auction.type} {auction.model}</h3>
         <p className={'font-semibold text-sm'}>{auction.year}</p>
       </div>
+      
     </a>
   )
 }
--- frontend/web-app/next.config.js
+++ frontend/web-app/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['1.bp.blogspot.com', 'res.cloudinary.com', 'www.gibson.com', 'cdn.pixabay.com', 'placeimg.com']
+    domains: ['1.bp.blogspot.com', 'res.cloudinary.com', 'www.gibson.com', 'cdn.pixabay.com', 'placeimg.com', 'images.ctfassets.net']
   }
 }
 
