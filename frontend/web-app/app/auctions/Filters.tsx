d89eed8171ac8a34e9740bf70917da6a52ca3c52
rafaelmmedeiros <rafael.einherjar@gmail.com>
2023-08-31 13:46:14 -0300

    Added order by

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,14 +1,37 @@
 ﻿import React from 'react';
 import {Button} from "flowbite-react";
 import {useParamsStore} from "@/hooks/useParamsStore";
+import {AiOutlineClockCircle, AiOutlineSortAscending} from "react-icons/ai";
+import {BsFillStopCircleFill} from "react-icons/bs";
 
 const pageSizeButtons = [4, 8, 16, 32];
-
+const orderByButtons = [
+  {label: 'Alphabetical', icon: AiOutlineSortAscending, value: 'brand', },
+  {label: 'Ending Date', icon: AiOutlineClockCircle, value: 'endingSoon'},
+  {label: 'Added Date', icon: BsFillStopCircleFill, value: 'new'},
+];
 export default function Filters() {
   const pageSize = useParamsStore(state => state.pageSize)
   const setParams = useParamsStore(state => state.setParams)
+  const orderBy = useParamsStore(state => state.orderBy)
   return (
     <div className={'flex justify-between items-center mb-4'}>
+      <div>
+        <span className={'uppercase text-sm text-gray-500 mr-2'}>Order By</span>
+        <Button.Group>
+          {orderByButtons.map(({label, icon: Icon, value}) => (
+            <Button
+              key={value}
+              onClick={() => setParams({orderBy: value})}
+              color={`${orderBy === value ? 'red' : 'gray'}`}
+              className={'focus:ring-0'}
+            >
+              <Icon className={'mr-2 h-5 w-4'}/>
+              {label}
+            </Button>
+          ))}
+        </Button.Group>
+      </div>
       <div>
         <span className={'uppercase text-sm text-gray-500 mr-2'}>Page Size</span>
         <Button.Group>
