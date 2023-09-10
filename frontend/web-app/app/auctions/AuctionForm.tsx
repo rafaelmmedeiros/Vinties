fcb906751eefa12ed7d65591fb43f2e9ce61a560
rafaelmmedeiros <rafael.einherjar@gmail.com>
2023-09-10 11:08:11 -0300

    Added input generic component

---
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -0,0 +1,28 @@
+﻿'use client'
+
+import React from "react";
+import {FieldValues, useForm} from "react-hook-form";
+import {Button} from "flowbite-react";
+import Input from "@/app/components/Input";
+
+export default function AuctionForm() {
+  const {control, handleSubmit, setFocus, 
+    formState: {isSubmitting, isValid, isDirty, errors}} = useForm()
+  
+  function onSubmit(data: FieldValues) {
+    console.log(data)
+  }
+  
+  return (
+    <form className={'flex flex-col mt-3'} onSubmit={handleSubmit(onSubmit)}>
+      
+      <Input label={'Brand'} name={'brand'} control={control} rules={{required: 'Brand is required'}}/>
+      <Input label={'Model'} name={'model'} control={control} rules={{required: 'Model is required'}}/>
+      
+      <div className={'flex justify-between'}>
+        <Button outline color={'gray'}>Cancel</Button>
+        <Button type={'submit'} isProcessing={isSubmitting} outline color={'success'}>Submit</Button>
+      </div>
+    </form>
+  )
+}
--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,7 +1,12 @@
 ﻿import React from "react";
+import Heading from "@/app/components/Heading";
+import AuctionForm from "@/app/auctions/AuctionForm";
 
 export default function Create() {
   return (
-    <div>Create Auction Page</div>
+    <div className={'mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg'}>
+      <Heading title={'Sell your vintie!'} subtitle={'Please enter the details'}/>
+      <AuctionForm/>
+    </div>
   );
 } 
