9efc68a445e088d4b31bd99c1be0793962b30f28
rafaelmmedeiros <rafael.einherjar@gmail.com>
2023-09-10 09:52:46 -0300

    Create filters for seller and winners

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -8,35 +8,61 @@ import Link from "next/link";
 import {AiFillTrophy, AiOutlineLogout} from "react-icons/ai";
 import {GiGuitar} from "react-icons/gi";
 import {signOut} from "next-auth/react";
+import {usePathname, useRouter} from "next/navigation";
+import {useParamsStore} from "@/hooks/useParamsStore";
 
 type Props = {
-  user: Partial<User>
+  user: User
 }
 
 export default function UserActions({user}: Props) {
+  const router = useRouter()
+  const pathname = usePathname()
+  const setParams = useParamsStore(state => state.setParams)
+  
+  function setWinner() {
+    setParams({winner: user.username, seller: undefined})
+    if(pathname !== '/') {
+      router.push('/')
+    }
+  }
+
+  function setSeller() {
+    setParams({seller: user.username, winner: undefined})
+    if(pathname !== '/') {
+      router.push('/')
+    }
+  }
+  
   return (
     <Dropdown
       inline
       label={`Welcome ${user.name}`}
     >
-      <Dropdown.Item icon={HiUser}>
-        <Link href={'/'}>My auctions</Link>
+      <Dropdown.Item icon={HiUser} onClick={setSeller}>
+        My auctions
       </Dropdown.Item>
-      <Dropdown.Item icon={AiFillTrophy}>
-        <Link href={'/'}>Auctions won</Link>
+      
+      <Dropdown.Item icon={AiFillTrophy} onClick={setWinner}>
+        Auctions won
       </Dropdown.Item>
+      
       <Dropdown.Item icon={GiGuitar}>
         <Link href={'/auctions/create'}>Sell a vintie</Link>
       </Dropdown.Item>
+      
       <Dropdown.Item icon={HiOutlineCog6Tooth}>
         <Link href={'/session'}>Session (dev)</Link>
       </Dropdown.Item>
+      
       <Dropdown.Divider/>
+      
       <Dropdown.Item icon={AiOutlineLogout}
         onClick={() => signOut({callbackUrl: '/'})}
       >
         Sign out
       </Dropdown.Item>
+      
       <Dropdown.Divider/>
     </Dropdown>
   )
--- frontend/web-app/hooks/useParamsStore.ts
+++ frontend/web-app/hooks/useParamsStore.ts
@@ -8,6 +8,8 @@ type State = {
   searchValue: string
   orderBy: string
   filterBy: string
+  seller?: string
+  winner?: string
 }
 
 type Actions = {
@@ -23,7 +25,9 @@ const initialState: State = {
   searchTerm: '',
   searchValue: '',
   orderBy: 'brand',
-  filterBy: 'live'
+  filterBy: 'live',
+  seller: undefined,
+  winner: undefined
 }
 
 export const useParamsStore = create<State & Actions>()(
